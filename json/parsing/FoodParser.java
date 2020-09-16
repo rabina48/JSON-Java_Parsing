ec4d9eee30d11f4857ddebce608caeb9aac487c0
rabina48 <45580944+rabina48@users.noreply.github.com>
2020-09-16 17:26:32 +0545

    Add files via upload

--- json/parsing/FoodParser.java
+++ json/parsing/FoodParser.java
@@ -25,16 +25,16 @@ public class FoodParser {
     protected   Food parseFood() throws IOException {
         Batter batter = new Batter();
         List<Batter> batterobj = new ArrayList<>();
-        batter.id = batterArr.toString();
-        batter.type =batterArr.toString();
+        batter.id = batterArr.getJSONObject(1).getString("id");
+        batter.type =batterArr.getJSONObject(1).getString("type");
 
         Food food = new Food();
         JSONObject batters = new JSONObject(object.getJSONObject("batters"));
 
         Topping topping = new Topping();
         List<Food> toppingObj = new ArrayList<>();
-        topping.id = jsonArray.toString();
-        topping.type = jsonArray.toString();
+        topping.id = jsonArray.getJSONObject(1).getString("id");
+        topping.type = jsonArray.getJSONObject(1).getString("type");
         toppingObj.add(food);
 
 
@@ -49,7 +49,11 @@ public class FoodParser {
         System.out.println("The Type is = " + food.type);
         System.out.println("The name is = " + food.name);
         System.out.println("The price per use is =" + food.ppu);
-        System.out.println("The battter = " + food.batters);
+        System.out.println("The battters = " + food.batters);
+        System.out.println("The batter = " + batter.id+" and its type is = "+batter.type);
+        System.out.println("The topping = " + topping.id+" and type is = "+topping.type);
+
+
 
         Food foodStringValue = new Food(food.id, food.type, food.name, food.ppu, batters);
         return foodStringValue;
@@ -59,19 +63,20 @@ public class FoodParser {
 
 
         for (int i = 0; i <batterArr.length(); i++) {
+            System.out.println("===========================================");
+            System.out.println("Batters id = "+(batterArr.getJSONObject(i)).get("id"));
+            System.out.println(" and  type ="+(batterArr.getJSONObject(i)).get("type"));
         }
-       System.out.println("===========================================");
-       System.out.println("batters id = "+(batterArr.getJSONObject(1)).get("id")+" and batters type ="+(batterArr.getJSONObject(1)).get("type"));
+
        System.out.println();
-
-
-
         for(int i = 0; i < jsonArray.length(); i++){
-
-            System.out.println(jsonArray.getJSONObject(1).getString("id"));
-            System.out.println(jsonArray.getJSONObject(1).getString("type"));
+            System.out.println("Topping id = "+jsonArray.getJSONObject(i).getString("id"));
+            System.out.println(" and type is ="+jsonArray.getJSONObject(i).getString("type"));
         }
        System.out.println("===========================================");
+       // String val =jsonArray.getJSONObject(1).getString("id");
+
+       System.out.println();
     }
 
 
--- json/parsing/Topping.java
+++ json/parsing/Topping.java
@@ -1,5 +1,7 @@
 package json.parsing;
 
+import org.json.JSONArray;
+
 import java.util.List;
 
 public class Topping {
